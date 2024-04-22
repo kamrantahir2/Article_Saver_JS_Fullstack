32f0585e7977005ad4aa8394adee42f3d28de82f
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-22 21:18:18 +0100

    Add ability to add articles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 let articles = [
   {
     id: 1,
@@ -19,11 +21,18 @@ let articles = [
   {
     id: 3,
     title: "Understanding Redux: A tutorial with examples",
-    descirption: "Redux tutorial",
+    description: "Redux tutorial",
     url: "https://blog.logrocket.com/understanding-redux-tutorial-examples/",
     favourite: true,
   },
 ];
+
+const generateId = () => {
+  const maxId =
+    articles.length > 0 ? Math.max(...articles.map((a) => a.id)) : 0;
+
+  return maxId + 1;
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -48,6 +57,28 @@ app.delete("/api/articles/:id", (request, response) => {
   const id = Number(request.params.id);
   articles = articles.filter((a) => a.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/articles", (request, response) => {
+  const body = request.body;
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({
+      error: "missing content",
+    });
+  }
+
+  const article = {
+    id: generateId(),
+    title: body.title,
+    description: body.description,
+    url: body.url,
+    favourite: Boolean(body.favourite) || false,
+  };
+
+  articles = articles.concat(article);
+
+  response.json(article);
 });
 
 const PORT = 3001;
