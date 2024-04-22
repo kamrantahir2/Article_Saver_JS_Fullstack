695ca4743b02596b6374e778fd1512a764176285
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-22 19:42:35 +0100

    Add initial array of articles

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import express from "express";
 const app = express();
+
+let articles = [
+  {
+    title: "Using the Postgres.js library",
+    description: "How to use PostgreSQL with JS",
+    url: "https://dev.to/opeoginni/how-to-use-the-postgresjs-library-jh",
+    favourite: true,
+  },
+  {
+    title: "How to use JSON Web Token (JWT) in Node.js",
+    description: "JSON Web Token tutorial",
+    url: "https://codedamn.com/news/nodejs/use-json-web-token-jwt-in-nodejs",
+    favourite: false,
+  },
+  {
+    title: "Understanding Redux: A tutorial with examples",
+    descirption: "Redux tutorial",
+    url: "https://blog.logrocket.com/understanding-redux-tutorial-examples/",
+    favourite: true,
+  },
+];
