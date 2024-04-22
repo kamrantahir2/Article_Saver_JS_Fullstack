f642edad549145d848462fa22ce0894c4ffe9fd2
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-22 20:45:16 +0100

    Chaneg scripts, add id's to articles

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ const app = express();
 
 let articles = [
   {
+    id: 1,
     title: "Using the Postgres.js library",
     description: "How to use PostgreSQL with JS",
     url: "https://dev.to/opeoginni/how-to-use-the-postgresjs-library-jh",
     favourite: true,
   },
   {
+    id: 2,
     title: "How to use JSON Web Token (JWT) in Node.js",
     description: "JSON Web Token tutorial",
     url: "https://codedamn.com/news/nodejs/use-json-web-token-jwt-in-nodejs",
     favourite: false,
   },
   {
+    id: 3,
     title: "Understanding Redux: A tutorial with examples",
     descirption: "Redux tutorial",
     url: "https://blog.logrocket.com/understanding-redux-tutorial-examples/",
