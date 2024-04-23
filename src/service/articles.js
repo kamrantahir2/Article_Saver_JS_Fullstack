67ee25ee4db47f5ac0c01d7e8b884acc167f1a73
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-23 17:46:03 +0100

    Create service for for frontend in articles.js

---
+++ src/service/articles.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/articles";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then(response.data);
+};
+
+return { getAll, create, update };
