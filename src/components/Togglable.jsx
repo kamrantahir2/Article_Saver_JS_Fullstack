a383c0889bbeb540160406e1a159bbc0a0910e99
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-24 22:38:48 +0100

    Start Togglable component

---
+++ src/components/Togglable.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const Togglable = (props) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Togglable;
