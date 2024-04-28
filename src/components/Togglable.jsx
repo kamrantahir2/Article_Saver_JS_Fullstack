3134c9694d0e5815beec20facc81f1374855abed
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-28 22:15:44 +0100

    Finish Togglable component

--- src/components/Togglable.jsx
+++ src/components/Togglable.jsx
@@ -12,7 +12,13 @@ const Togglable = (props) => {
 
   return (
     <div>
-      <div></div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
     </div>
   );
 };
