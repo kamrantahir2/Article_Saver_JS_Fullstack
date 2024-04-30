90dc2951f45de7dad4dbd6d42372cc16c63177de
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-30 19:14:40 +0100

    fIX ISSUE WITH .__v propery of userSchema not being deleted through toJSON

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -20,7 +20,7 @@ userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
-    delete returnedObject._v;
+    delete returnedObject.__v;
     delete returnedObject.passwordHash;
   },
 });
