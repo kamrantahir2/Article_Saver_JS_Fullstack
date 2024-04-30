8c6fc8ac31acf10917e6c83acff176c06a53d902
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-30 18:59:21 +0100

    Make username unique and required

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: String,
   passwordHash: String,
   articles: [
