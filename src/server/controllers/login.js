d21f8aa12373cde257378da186fd8b660c10faa3
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-04-30 23:01:30 +0100

    Create login controller for logging in as user

---
+++ src/server/controllers/login.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import express from "express";
+import User from "../models/user";
+import dotenv from "dotenv";
+dotenv.config();
+const loginRouter = express.Router();
+
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
+
+  const user = await User.findOne({ username });
+
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.json(401).json({ error: "invalid username or password" });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
+
+export default loginRouter;
