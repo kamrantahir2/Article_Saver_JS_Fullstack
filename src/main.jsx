0c694320405ec708248cf4ef0719ddccbb5df671
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-05-15 16:01:15 +0100

    Change to hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  BrowserRouter as Router,
+} from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
